96b8bad18541729645b1ee031e95b187b34b0ce4
howardc72150 <52299899+howardc72150@users.noreply.github.com>
2021-02-26 11:30:28 +1300

    Update 06_temperature_converter.py

--- 01_temp_conversion/06_temperature_converter.py
+++ 01_temp_conversion/06_temperature_converter.py
@@ -11,18 +11,111 @@ class Converter:
         background_colour = "light blue"
 
         # Converter frame
-        self.converter_Frame = Frame(bg=background_colour, pady=10)
-        self.converter_Frame.grid()
+        self.converter_frame = Frame(bg=background_colour, pady=10)
+        self.converter_frame.grid()
 
         # Temperature converter heading (row=0)
-        self.temp_heading_label = Label(self.converter_Frame, text="Temperature Converter",
+        self.temp_heading_label = Label(self.converter_frame, text="Temperature Converter",
                                         font=("Verdana", "19", "bold"), bg=background_colour,
                                         pady=10, padx=10)
         self.temp_heading_label.grid(row=0)
 
         # User instructions (row=1)
-        self.temp_instructions_label = Label(self.converter_Frame,
+        self.temp_instructions_label = Label(self.converter_frame,
                                              text="Type in the amount to be converted"
                                              " and then push one of the buttons below",
                                              font=("Verdana", "15"), wrap=290, justify=LEFT,
                                              bg=background_colour, padx=10, pady=10)
+        self.temp_instructions_label.grid(row=1)
+
+        # Temperature entry box (row=2)
+        self.to_convert_entry = Entry(self.converter_frame, width=20, font=("Verdana", "14"))
+        self.to_convert_entry.grid(row=2)
+
+        # Conversion buttons frame (row 3)
+        self.conversion_buttons_frame = Frame(self.converter_frame)
+        self.conversion_buttons_frame.grid(row=3, pady=10)  
+        self.to_c_button = Button(self.conversion_buttons_frame, text="To Centigrade",
+                                  font=("Verdana", "10", "bold"), padx=10, pady=10,
+                                  bg="Khaki1", command=lambda: self.temp_convert(-459))
+        self.to_c_button.grid(row=0, column=0)
+
+        self.to_f_button = Button(self.conversion_buttons_frame, text="To Fahrenheit", 
+                                  font=("Verdana", "10", "bold"), bg="Orchid1", padx=10,
+                                  pady=10, command=lambda: self.temp_convert(-273))
+        self.to_f_button.grid(row=0,column=1)
+
+        # Answer label (row 4)
+        self.converted_label = Label(self.conversion_buttons_frame, font=("Verdana", "10", "bold"),
+                                     fg="purple", bg=background_colour, pady=10,
+                                     text="Conversion goes here")
+        self.converted_label.grid(row=4)
+
+        # History / Help button frame (row 5)
+        self.hist_help_frame = Frame(self.converter_frame)
+        self.hist_help_frame.grid(row=5, pady=10)
+
+        self.calc_hist_button = Button(self.hist_help_frame, font=("Verdana", "10", "bold"),
+                                       text="Calculation History", width=15)
+        self.calc_hist_button.grid(row=0, column=0)
+
+        self.help_button = Button(self.hist_help_frame, font=("Verdana", "10", "bold"),
+                                  text="Help", width=5)
+        self.help_button.grid(row=0, column=1)
+
+    def temp_convert(self, low):
+        error_colour = "#ffafaf"
+
+        # Retrieve amount entered into entry field
+        to_convert = self.to_convert_entry.get()
+
+        try:
+            to_convert = float(to_convert)
+            has_errors = False
+
+            # Check and convert to fahrenheit
+            if low == -273 and to_convert >= low:
+                fahrenheit = (to_convert * 9/5) + 32
+                to_convert = self.round_it(to_convert)
+                fahrenheit = self.round_it(fahrenheit)
+                answer = "{} degrees C is {} degrees F".format(to_convert, fahrenheit)
+            
+            # Check and convert to centigrade
+            elif low == -459 and to_convert >= low:
+                celsius = (to_convert - 32) * 5/9
+                to_convert = self.round_it(to_convert)
+                celsius = self.round_it(celsius)
+                answer = "{} degrees C is {} degrees F".format(to_convert, celsius)
+            
+            else:
+                answer = "Too cold"
+                has_errors = True
+            
+            # Display answer
+            if has_errors == False:
+                self.converted_label.configure(text=answer, fg="blue")
+                self.to_convert_entry.configure(bg="white")
+            else:
+                self.converted_label.configure(text=answer, fg="red")
+                self.to_convert_entry.configure(bg=error_colour)
+            
+            # Add answer to list for history
+        
+        except ValueError:
+            self.converted_label.configure(text="Enter a number", fg="red")
+            self.to_convert_entry.configure(bg=error_colour)
+    
+    def round_it(self, to_round):
+        if to_round % 1 == 0:
+            rounded = int(to_round)
+        else:
+            rounded = round(to_round, 1)
+        
+        return rounded
+
+# Main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    var = Converter()
+    root.mainloop()
