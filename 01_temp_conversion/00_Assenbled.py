0ee073a1a527e1391a3b3e043a1d7aadbfcf7db9
howardc72150 <52299899+howardc72150@users.noreply.github.com>
2021-03-15 10:51:29 +1300

    Updated

--- 01_temp_conversion/00_Assenbled.py
+++ 01_temp_conversion/00_Assenbled.py
@@ -184,7 +184,7 @@ class History:
 
         # Export button
         self.export_button = Button(self.export_dismiss_frame, text="Export",
-                                    font=("Verdana", "10", "bold"))
+                                    font=("Verdana", "10", "bold"), command=self.export)
         self.export_button.grid(row=0, column=0)
 
         # Dismiss button
@@ -192,11 +192,75 @@ class History:
                                      font=("Verdana", "10", "bold"), 
                                      command=partial(self.close_history, partner))
         self.dismiss_button.grid(row=0, column=1)
-                                     
+
+    def export(self):
+        get_export = Export(self)
+
     def close_history(self, partner):
         # Put history button back to normal
         partner.history_button.config(state=NORMAL)
         self.history_box.destroy()
+
+class Export:
+    def __init__(self, partner):
+        background = "#a9ef99"       # --  Pale green
+
+        # Disable export button
+        partner.export_button.config(state=DISABLED)
+
+        # Sets up child window
+        self.export_box = Toplevel()
+
+        # If users press cross at top, closes export
+        # and releases export button.
+        self.export_box.protocol('WM_DELETE_WINDOW', 
+                                 partial(self.close_export, partner))
+        
+        # Set up GUI frame
+        self.export_frame = Frame(self.export_box, width=3000, bg=background)
+        self.export_frame.grid()
+
+        # Set up export heading (row 0)
+        self.how_heading = Label(self.export_frame,
+                                 text="Export and Instructions",
+                                 font=("Verdana", "14", "bold"),
+                                 bg=background)
+        self.how_heading.grid(row=0)
+
+        # Export instrutcions (label, row 1)
+        self.export_text = Label(self.export_frame, text="Enter a filename in the box"
+                                 " below and press the save button to save your calculation"
+                                 " history to a text file.",
+                                 justify=LEFT, width=10, wrap=250, bg=background)
+        #self.export_text.grid(row=1)
+
+        # Warning text (label, row 2)
+        self.export_text = Label(self.export_frame, text="If the filename you enter below"
+                                " already exists, its contents will be replaced with your"
+                                " calculation history", justify=LEFT, bg="#ffafaf", fg="maroon",
+                                font=("Verdana", "10", "italic"), wrap=225, padx=10, pady=10)
+        self.export_text.grid(row=2, pady=10)
+
+        # Filename entry box (row 3)
+        self.filename_entry = Entry(self.export_frame, width=20,font=("Verdana", "10", "italic"),
+                                    justify=CENTER)
+        self.filename_entry.grid(row=3, pady=10)
+
+        # Save and cancel frame (row 4)
+        self.save_cancel_frame = Frame(self.export_frame)
+        self.save_cancel_frame.grid(row=5, pady=10)
+
+        # Save and cancel buttons (row 0 of save_Cancel frame)
+        self.save_button = Button(self.save_cancel_frame, text="Save")
+        self.save_button.grid(row=0, column=0)
+
+        self.cancel_button = Button(self.save_cancel_frame, text="Cancel",
+                                    command=partial(self.close_export, partner))
+        self.cancel_button.grid(row=0, column=1)
+
+    def close_export(self, partner):
+        partner.export_button.config(state=NORMAL)
+        self.export_box.destroy()
 
 class Help:
     def __init__(self, partner):
